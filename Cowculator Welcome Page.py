d2493a0e6fe7a0a9ed2387bc579e2dae89b73ed6
Han Mai <75762382+mhanniem@users.noreply.github.com>
2021-11-17 12:03:15 -0800

    Update Cowculator Welcome Page.py

--- Cowculator Welcome Page.py
+++ Cowculator Welcome Page.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import simpledialog
 
 def first_page():
     first_page = Tk()
@@ -31,7 +32,6 @@ def second_page():
 
 
 
-
     calculate_footprint = Button(page2, text = "Calculate your footprint" ,
                  fg = "green", command = lambda:[page2.destroy(), third_page()])
     calculate_footprint.place(relx=0.5,rely=0.5, anchor = CENTER)
@@ -42,38 +42,72 @@ def third_page():
     page3.title("Calculating...")
     page3.geometry('500x500')
 
+   
 
-    ask_acreage= Label(page3, text = "Input your farm's acreage")
-    ask_acreage.place(relx=.5,rely=.2,anchor = CENTER)
+    acre_lbl = Label(page3, text = "How many acres?")
+    acre_lbl.place(relx=.5,rely=.1,anchor = CENTER)
+    
+    acre_answer = Entry(page3, width=10)
+    acre_answer.place(relx=.5,rely=.2,anchor = CENTER)
 
+    
+        
+    acre_btn = Button(page3, text = "acres" ,fg = "red", command=acres_clicked)
+    acre_btn.place(relx=0.5,rely=0.3, anchor = CENTER)
 
 
     
-    #Access the Menu Widget using StringVar function
-    access= StringVar()
-    #Create an instance of Menu in the frame
-    main_menu = OptionMenu(page3, access, "500-1000", "1001-", "Python", "Rust","Go","Ruby")
-    main_menu.pack()
+
+    animal_lbl = Label(page3, text = "What type of animal?")
+    animal_lbl.place(relx=.5,rely=.4,anchor = CENTER)
+    
+    #animal_answer = Entry(page3, width=10)
+    #animal_answer.place(relx=.5, rely=.5, anchor = CENTER)
+    
+    #animal_btn = Button(page3, text = "animal type" ,fg = "red", command=animal_clicked)
+    #animal_btn.place(relx=0.5,rely=0.6, anchor = CENTER)
+
+    animal_type = StringVar()
+    animal_menu = OptionMenu(page3, animal_type, "Goats", "Cattle", "Chickens", "Sheeps","Pigs")
+    animal_menu.place(relx=.5, rely=.5, anchor = CENTER)
 
     
-    input_acreage = Entry(page3, width=10)
-    input_acreate.place(relx=.5, rely=.35,anchor = CENTER)
+    #def animal_clicked():
+    #res = "You chose " + animal_answer.get()
+    #lbl.configure(text = res)
+        #animal_type = animal_answer.get()
 
+    
+    
+    animal_num_lbl = Label(page3, text = "How many?")
+    animal_num_lbl.place(relx=.5,rely=.7,anchor = CENTER)
+    
+    animal_num = Entry(page3, width=10)
+    animal_num.place(relx=.5,rely=.8,anchor = CENTER)
+    
+
+    animal_num_btn = Button(page3, text = "animal number" , fg = "red", command=animal_num_clicked)
+    animal_num_btn.place(relx=0.5,rely=0.9, anchor = CENTER)
+    
+
+def acres_clicked():
+    #res = "You have " + acre_answer.get() + "acres."
+    #lbl.configure(text = res)
+    acre = acre_answer.get()
+
+def animal_num_clicked():
+    #res = "You have " + animal_num.get() +animal_answer.get()
+    #lbl.configure(text = res)
+    animal_num = animal_num.get()  
+
+    
+def main():
+    acre = 0
+    animal_type = ''
+    animal_num = 0
   
 
-    def clicked():
-        res = "You have a total of" + txt.get() + "acres."
-        ask_acreage.configure(text = res)
-
-    btn3 = Button(page3, text="Calculate", fg="green", command=clicked())
-    btn3.place(relx=0.5,rely=0.5, anchor = CENTER)
-
-    btn4 = Button(page3, text = "Which animal?" ,
-                 fg = "red", command = lambda:[root3.destroy()])
-    btn4.place(relx=0.5,rely=0.5, anchor = CENTER)
-    
-
-def main():
+    answer = 0
     first_page()
 
 main()
